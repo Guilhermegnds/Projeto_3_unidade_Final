3920ca7352d9778e5e3544404ae9e92c8c04889f
Guilhermegnds <guilherme.gnds@gmail.com>
2019-06-23 17:06:33 -0300

    Projeto Final

--- Projeto_3_unidade/matrizes.c
+++ Projeto_3_unidade/matrizes.c
@@ -11,7 +11,7 @@ struct matrizes randmat (int nlinhas, int ncolunas){
     srand(time(NULL));
     for (i=0;i<m.nlinhas;i++) {
         for (j=0;j<m.ncolunas;j++) {
-            m.m[i][j] = rand();
+            m.m[i][j] = rand()% 50;
         }
     }
     return m;
@@ -25,7 +25,7 @@ struct matrizes atriVal (int nlinhas, int ncolunas){
     for (i=0;i<m.nlinhas;i++) {
         for (j=0;j<m.ncolunas;j++) {
             printf("Digite o elemento (%d,%d) da Matriz : ", i, j);
-            scanf("%f", &m.m[i][j]);
+            scanf("%.1f", &m.m[i][j]);
         }
     }
     return m;
@@ -34,7 +34,7 @@ struct matrizes imprimir (struct matrizes m){
     int i, j;
     for (i=0;i<m.nlinhas;i++) {
         for (j=0;j<m.ncolunas;j++) {
-            printf("%f ", m.m[i][j]);
+            printf("%.1f ", m.m[i][j]);
         }
         printf("\n");
     }
@@ -47,19 +47,17 @@ struct matrizes arquivos(void){
     FILE *file;
     a[0]=0;
     printf("Digite a localizacao do arquivo: ");
- //   printf("tam = %d\n", strlen(a));
     while(strlen(a) < 2){
-       fgets(a, 100, stdin);
+        fgets(a, 100, stdin);
     }
     a[strlen(a)-1]= 0;
     file = fopen(a, "r");
     fscanf(file,"%d %d", &n.nlinhas, &n.ncolunas);
-//    printf("%d %d\n", n.nlinhas, n.ncolunas);
     for(i=0; i<n.nlinhas; i++){
         for( j=0; j<n.ncolunas; j++){
-           fscanf(file, "%f", &n.m[i][j]);
+            fscanf(file, "%f", &n.m[i][j]);
         }
-      }
+    }
     return n;
 }
 
@@ -69,19 +67,18 @@ struct matrizes impArquivos(struct matrizes m){
     FILE *fileout;
     a[0]=0;
     printf("Digite a localizacao de destino do arquivo: ");
- //   printf("tam = %d\n", strlen(a));
     while(strlen(a) < 2){
-       fgets(a, 100, stdin);
-   }
+        fgets(a, 100, stdin);
+    }
     a[strlen(a)-1]= 0;
     fileout = fopen(a, "w");
     fprintf(fileout,"%d %d\n", m.nlinhas, m.ncolunas);
     for(i=0; i<m.nlinhas; i++){
         for( j=0; j<m.ncolunas; j++){
-           fprintf(fileout, "%4f ", m.m[i][j]);
+            fprintf(fileout, "%4.1f ", m.m[i][j]);
         }
         fprintf(fileout, "\n");
-      }
+    }
     fclose(fileout);
 }
 
--- Projeto_3_unidade/matrizes.h
+++ Projeto_3_unidade/matrizes.h
@@ -3,6 +3,7 @@ struct matrizes {
     int ncolunas;
     float m[100][100];
 };
+struct matrizes atriTam (struct matrizes m);
 struct matrizes randmat (int nlinhas, int ncolunas);
 struct matrizes atriVal (int nlinhas, int ncolunas);
 struct matrizes imprimir (struct matrizes m);
